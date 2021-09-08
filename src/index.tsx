e7c38fa259b875091c0504ed78315a04fe5ca580
Ansat Jartybayev <ansatjr@gmail.com>
2021-09-09 03:10:44 +0600

    init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {App} from "./app";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
